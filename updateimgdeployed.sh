a9ffb1b8ede502fecc5542f1f59ca700e81af1b8
Ken Goddard <closerwalk@gmail.com>
2022-09-03 10:29:45 -0700

    Update updateimgdeployed.sh

--- updateimgdeployed.sh
+++ updateimgdeployed.sh
@@ -6,10 +6,12 @@ then
 IMAGENAME=$(grep "image" ./deployment/green/deploymentgreen.yaml)
 sed -i 's/blue/green/g' latest.txt
 sed -i "s|$IMAGENAME|$1|g" ./deployment/green/deploymentgreen.yaml
+sed -i "s|"bluebg.jpg"|"greenbg.jpg"|g" ./$HOME/blue-green/html/index.html
 echo "Next Deployment will be Green"
 else
 IMAGENAME=$(grep "image" ./deployment/blue/deploymentblue.yaml)
 sed -i 's/green/blue/g' latest.txt
 sed -i "s|$IMAGENAME|$1|g" ./deployment/blue/deploymentblue.yaml
+sed -i "s|"greenbg.jpg"|"bluebg.jpg"|g" ./$HOME/blue-green/html/index.html
 echo "Next Deployment will be Blue"
 fi
